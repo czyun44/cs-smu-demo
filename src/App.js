6fa2a2ac3a8ef9009c4fea4a8f208376fe1362fb
VurhD <benegaldhruv2312@gmail.com>
2023-06-23 20:27:47 +0800

    GGWP

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
+import { useState } from 'react';
+import emojis from './resources/emojis.json';
+import './App.css';
+import styled from 'styled-components';
+import {Input, input} from 'semantic-ui-react';
+
 export const App = () => {
-  return <div>Hi there</div>;
+  const [text, setText] = useState("");
+
+  const filteredEmojis = emojis.filter(emoji => (emoji.name.includes(text)))
+
+  console.log("text is:", text);
+
+  return (
+  <div className="AppWrapper">
+    <div>
+    <Input onChange = {(e, data) => setText(data.value)}
+    icon={{name: 'search', circular: true, link: true}}
+    placeholder="Search..."
+    fluid
+    />
+    </div>
+    {filteredEmojis.map((emoji) => (
+      <EmojiRow emoji={emoji} key={emoji.code} />
+    ))}
+  </div>
+  )
 };
+
+const EmojiRow = ({ emoji }) => (
+  <div className="RowWrapper">
+      <div className="CharNameWrapper">
+        <div className="EmojiInfo">{emoji.character}</div>
+        <div className="EmojiInfo">{emoji.name}</div>
+      </div>
+      <div className="EmojiInfo">{emoji.code}</div>
+    </div>
+);
