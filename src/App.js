009248b6935e865816829aecb07e1aaff120a943
Chua Zhi Yun <42872909+czyun44@users.noreply.github.com>
2023-06-23 21:17:40 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -2,37 +2,51 @@ import { useState } from 'react';
 import emojis from './resources/emojis.json';
 import './App.css';
 import styled from 'styled-components';
-import {Input, input} from 'semantic-ui-react';
+import { Input, input } from 'semantic-ui-react';
+import { wait } from '@testing-library/user-event/dist/utils';
+import debounce from 'lodash.debounce';
 
 export const App = () => {
-  const [text, setText] = useState("");
+  const [searchText, setsearchText] = useState("");
+  const [fetchedEmojis, setFetchedEmoji] = useState([]);
 
-  const filteredEmojis = emojis.filter(emoji => (emoji.name.includes(text)))
+  const getEmoji = async (text) => {
+    const url = `https://emoji-api.com/emojis?search=${text}&access_key=cc28cbf09d8bf40c8c66611902652e768a28b198`;
+    const res = await fetch(url)
+    const emojis = await res.json()
+    console.log("fetched emojis are :", emojis);
+    setFetchedEmoji(emojis);
+  }
 
-  console.log("text is:", text);
+  const debounceEmojiSearch = debounce(
+    (searchText) => getEmoji(searchText),500
+  );
 
   return (
-  <div className="AppWrapper">
-    <div>
-    <Input onChange = {(e, data) => setText(data.value)}
-    icon={{name: 'search', circular: true, link: true}}
-    placeholder="Search..."
-    fluid
-    />
+    <div className="AppWrapper">
+      <div>
+        <Input onChange={async (e, data) => 
+        // await getEmoji(data.value)
+        await debounceEmojiSearch(data.value)
+      }
+          icon={{ name: 'search', circular: true, link: true }}
+          placeholder="Search..."
+          fluid
+        />
+      </div>
+      {fetchedEmojis.map((emoji) => (
+        <EmojiRow emoji={emoji} key={emoji.codePoint} />
+      ))}
     </div>
-    {filteredEmojis.map((emoji) => (
-      <EmojiRow emoji={emoji} key={emoji.code} />
-    ))}
-  </div>
   )
 };
 
 const EmojiRow = ({ emoji }) => (
   <div className="RowWrapper">
-      <div className="CharNameWrapper">
-        <div className="EmojiInfo">{emoji.character}</div>
-        <div className="EmojiInfo">{emoji.name}</div>
-      </div>
-      <div className="EmojiInfo">{emoji.code}</div>
+    <div className="CharNameWrapper">
+      <div className="EmojiInfo">{emoji.character}</div>
+      <div className="EmojiInfo">{emoji.unicodeName}</div>
     </div>
+    <div className="EmojiInfo">{emoji.codePoint}</div>
+  </div>
 );
